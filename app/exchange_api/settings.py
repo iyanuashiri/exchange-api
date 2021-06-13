6c532e7dcf576371a7944293ab64f37716eb0a41
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2021-06-13 18:46:12 +0100

    chore: dockerize the task

--- app/exchange_api/settings.py
+++ app/exchange_api/settings.py
@@ -88,10 +88,15 @@ WSGI_APPLICATION = 'exchange_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": config("SQL_ENGINE"),
+        "NAME": config("SQL_DATABASE"),
+        "USER": config("SQL_USER"),
+        "PASSWORD": config("SQL_PASSWORD"),
+        "HOST": config("SQL_HOST"),
+        "PORT": config("SQL_PORT"),
     }
 }
 
